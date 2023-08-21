0371d54f8a7df6c935455f273c41e477668868aa
Luiz Tosta <luiz-@outlook.de>
2023-08-21 18:29:18 -0300

    fix: Add '/' to subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -31,7 +31,7 @@ const config = {
 
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = '/design-system'
+      config.base = '/design-system/'
     }
 
     return config

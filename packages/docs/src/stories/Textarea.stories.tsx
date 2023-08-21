3ebeb190572c8ce5a20403c62bff927aa8a44826
Luiz Tosta <luiz-@outlook.de>
2023-08-21 17:55:43 -0300

    feat: add core components

---
+++ packages/docs/src/stories/Textarea.stories.tsx
@@ -0,0 +1,34 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Textarea, TextareaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text Area',
+  component: Textarea,
+  tags: ['autodocs'],
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextareaProps>
+
+export const Primary: StoryObj<TextareaProps> = {
+  args: {
+    placeholder: 'Add any observations',
+  },
+}
+
+export const Disabled: StoryObj<TextareaProps> = {
+  args: {
+    disabled: true,
+  },
+}
